c7e5df1de84a8a5818d45d71a8c16d785cf30b92
aseani miller <milleraseani@gmail.com>
2021-12-26 22:22:00 -0800

    styling

--- src/components/technical.js
+++ src/components/technical.js
@@ -16,12 +16,12 @@ export default function() {
                 The machine is very simple you type one letter and an electrical signal travels between 3 rotors and then gets reflected by the reflector and travels back out as an output.
                 As you can see through the blueprint. The machine is heavily dependant on the reflector (The very last Rectangle/wheel at the very top) , 
                 with a badly configured reflector you will not be able to properly decipher the encoded key that you've typed in. The entire scrambling process is based on the 
-                electrical pathways, for instance lets use four letters as an example. In the source code for the Api I mimicked the electrical pathways by making the rotors into arrays with two items,
+                electrical pathways, for instance lets use four letters as an example. In the source code for the Api, I mimicked the electrical pathways by making the rotors into arrays with two items,
                 those items are two strings so ['abcd', 'badc']. In this example I'll only use one rotor and our reflector will be ['abcd','dcba'] in the reflector the two strings must only have 1 unique pair, so if A maps to C then C must map to A.So lets say we type 'A', 'A' then finds the index of itself in the second item in the array/rotor which in this case would be 
                 1 it then finds the index 1 in the reflector which in this case is B and 'B' then finds itself in the second item in the reflector which will be 2. This is the part where the reflector plays a key role.
                 We now find the index of 2 in second item in the rotor array which will be D, so 'A' gets encrypted as 'D' if you now type in 'D' as your input the same process will happen and the reflector will reflect the opposite 
-                pathway to the rotors so the output would be 'A'. So in short 'A' =~ 1 =~ 'B' =~ 2 Reflects Back as D if we typed Input as 'D' =~ 2 =~ 'C' =~ 1 Reflects back as 'A' since A is array[1], If you take a look at the diagram you can see the encrypted text links directly with the input. 
-                This is how we are able to encrypt and decrypt messages without a encryption and decryption mode.  
+                pathway to the rotors so the output would be 'A'. So in short 'A' =~ 1 =~ 'B' =~ 2 Reflects Back as D if we typed the input as 'D' =~ 2 =~ 'C' =~ 1 Reflects back as 'A' since A is array[1], If you take a look at the diagram you can see the encrypted text links directly with the input. 
+                This is how we are able to encrypt and decrypt messages without an encryption and decryption mode.  
             </p>
          <h4>Computing All Possible Settings</h4>
          <p>With Permutations we can calculate the number of all possible settings : ((5! / (5-3)!)  * 37^3 )  *  (37! / (37 - 20)! * 2^10 * 10!) = 3.16 Septillion or 3.1649131 * 10^25 </p>
