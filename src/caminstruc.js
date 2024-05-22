f7307db7a6ccdac07e5ceadfe1a2a7de53b69bb4
Aseani Miller <aseanimiller@Aseanis-MacBook-Air.local>
2024-05-22 09:38:50 -0700

    updating

---
+++ src/caminstruc.js
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const CamInfo = () => {
+
+    return (<div className='caminfo' >
+
+   
+        <h2>Here are the instructions</h2>
+        <p>This part of the Application works on the same concept from earlier, The only difference is instead of typing the word in it will work primarly on visual data </p>
+         <p>Simply take a photo of the text you want to encrypt or decrypt and it will appear on your screen! Make sure you take a good photo! Do not photograph any text you don't want to encrypt/decrypt
+         </p>
+         
+        
+     
+     <h4>Click the Camera Icon when your ready!</h4>
+    
+    </div>)
+
+}
+
+export default CamInfo;
